7a09ebf40b361e3f018e470a4208f2719c757562
Kirill Monakhov <withewings0@gmail.com>
2025-07-10 18:51:06 +0300

    Changed keybinding for file picker

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -3,4 +3,4 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
 vim.keymap.set("n", "<space>td", ":ToggleTerm dir=%:p:h<CR>") -- open terminal in the directory of the file
 
-vim.keymap.set("n", "<space>vv", ":Drex<CR>") -- open terminal in the directory of the file
+vim.keymap.set("n", "<space>v", ":Drex<CR>") -- open terminal in the directory of the file
